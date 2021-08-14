d203da215275fdeb1957306021ecc18094ea1d4e
root <root@e7cfa4b601be>
2021-08-14 15:47:20 +0000

    change bash script

--- scripts/init_dirs.sh
+++ scripts/init_dirs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 # Create dirs for using with docker volumes
-mkdir -p /var/lib/postgresql \
-	/var/log/django \
+mkdir /var/lib/postgresql \
+        /var/log/django \
         /var/log/gunicorn \
-	/var/log/nginx \ 
-	/usr/src/www/assets \
-       	/usr/src/www/media \ 
-	/usr/src/www/static
+        /var/log/nginx \
+        /usr/src/www \
+        /usr/src/www/assets \
+        /usr/src/www/media \
+        /usr/src/www/static
 
 echo "Dirs is created"
 
 # Change group permissions
 chown -R :1101 /var/log/nginx
 
-chown -R :1102 
-        /var/log/django \
-        /var/log/gunicorn \ 
-	/usr/src/www/assets \
-	/usr/src/www/media \ 
-	/usr/src/www/static
+chown -R :1102 /var/log/django \
+        /var/log/gunicorn \
+        /usr/src/www/assets \
+        /usr/src/www/media \
+        /usr/src/www/static
 
 chown -R :1103 /var/lib/postgresql
 
@@ -27,10 +27,10 @@ echo "Owner group is changed"
 # Add permissions for group
 chmod -R 775 /var/lib/postgresql \
         /var/log/django \
-	/var/log/gunicorn \
-	/var/log/nginx \
-	/usr/src/www/assets \
-	/usr/src/www/media \
-	/usr/src/www/static
+        /var/log/gunicorn \
+        /var/log/nginx \
+        /usr/src/www/assets \
+        /usr/src/www/media \
+        /usr/src/www/static
 
 echo "Permissions for file by group is changed"
